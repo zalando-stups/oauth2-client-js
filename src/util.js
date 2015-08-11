8f7912ded8380330955e05fa650cc7db552bcd3e
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-08-11 17:54:31 +0200

    #1 oopsi

--- src/util.js
+++ src/util.js
@@ -1,9 +1,22 @@
+// https://github.com/sindresorhus/is-regexp
+function isRegex(re) {
+    return Object.prototype.toString.call(re) === '[object RegExp]';
+}
+
+function isString(s) {
+    return typeof s === 'string';
+}
+
+function identity(i) {
+    return i;
+}
+
 function assertPresent(obj, fields) {
     if (obj === undefined) {
         throw new Error();  //TODO message
     }
     fields = fields || [];
-    let undef = fields.filter( f => obj[f] === undefined );
+    let undef = fields.filter(f => obj[f] === undefined);
     if (undef.length) {
         throw new Error(`${undef[0]} is not present on {obj}.`);
     }
@@ -15,6 +28,26 @@ function includes(substring) {
 
 function startsWith(substring) {
     return this.indexOf(substring) === 0;
+}
+
+function encodeBase64(string) {
+    if (window && window.btoa) {
+        return window.btoa(string);
+    } else if (Buffer) {
+        return new Buffer(string).toString('base64');
+    } else {
+        throw new Error('Unsupported platform, can’t even Base64.');
+    }
+}
+
+function decodeBase64(string) {
+    if (window && window.atob) {
+        return window.atob(string);
+    } else if (Buffer) {
+        return new Buffer(string, 'base64').toString('ascii');
+    } else {
+        throw new Error('Unsupported platform, can’t even Base64.');
+    }
 }
 
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
@@ -31,10 +64,21 @@ function endsWith(searchString, position) {
 function stripKeys(obj, keys) {
     var stripped = {};
     keys = keys || [];
+
     Object
     .keys(obj)
     .forEach(key => {
-        if (keys.indexOf(key) < 0) {
+        let excludeKey = keys
+                            .map(exclude => {
+                                if (isString(exclude)) {
+                                    return key !== exclude;
+                                }
+                                if (isRegex(exclude)) {
+                                    return !exclude.test(key);
+                                }
+                            })
+                            .some(identity);
+        if (!excludeKey) {
             stripped[key] = obj[key];
         }
     });
